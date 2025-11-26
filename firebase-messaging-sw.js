6f36ae086069402863ddc6a84a568b8c75569a9e
CepteRandevu <kemal.garip@hotmail.com>
2025-11-26 10:26:30 +0300

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,29 @@
+// Firebase Messaging Service Worker
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
+// Senin config’in
 firebase.initializeApp({
-  apiKey: "AIzaSyB8jn673zFuYRDhgzFd5yfZ-MP2XSXgyFg",
+  apiKey: "AIzaSy... senin config",
   authDomain: "cepterandevu-35fe4.firebaseapp.com",
   projectId: "cepterandevu-35fe4",
-  storageBucket: "cepterandevu-35fe4.firebasestorage.app",
-  messagingSenderId: "86298180836",
-  appId: "1:86298180836:web:124c2004605d68b32700f1",
-  measurementId: "G-CZ159MMEE3"
+  storageBucket: "cepterandevu-35fe4.appspot.com",
+  messagingSenderId: "186298180836",
+  appId: "1:186298180836:web:124c004605d638230701"
 });
 
 const messaging = firebase.messaging();
 
+// Arka planda bildirim işleme
 messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/al/icons/icon-192.png"
-  });
+  console.log("[SW] Arka plan bildirimi geldi", payload);
+
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/al/icon.png",
+      badge: "/al/badge.png"
+    }
+  );
 });
